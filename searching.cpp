036c0321444378c1f63d0f9f015f9f71dd617cea
Xahoor72 <85664053+Xahoor72@users.noreply.github.com>
2021-09-30 12:08:00 +0530

    Update searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -367,6 +367,32 @@ which is 1.
         // Your code here
         
     }
+/*
+You are given an array of N+2 integer elements. All elements of the array are in range 1 to N. 
+Also, all elements occur once except two numbers which occur twice. Find the two repeating numbers.
 
+Example 1:
 
+Input:
+N = 4
+array[] = {1,2,1,3,4,3}
+Output: 1 3
+Explanation: In the given array, 
+1 and 3 are repeated two times.
 
+*/
+
+// your code here	
+vector<int> twoRepeated (int arr[], int N) {
+        // Your code here
+        vector<int> v;
+        for(int i=0;i<N+2;i++){
+            if(arr[abs(arr[i])]>0){
+                arr[abs(arr[i])]=-arr[abs(arr[i])];
+            }
+            else {
+                v.push_back(abs(arr[i]));
+            }
+        }
+        return v;
+    }
